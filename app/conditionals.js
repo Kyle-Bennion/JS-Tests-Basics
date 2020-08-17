0d802f1f0d30a975d89d3b4593337ea1f7bb0e03
Kyle Bennion <bilekennion@hotmail.com>
2020-08-17 16:19:53 -0600

    C 2 Complete

--- app/conditionals.js
+++ app/conditionals.js
@@ -18,7 +18,15 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
-
+    if (score <= 25 & score >= 23) {
+        return "A"
+    }
+    else if (score <= 10) {
+        return "F"
+    }
+    else {
+        return "C"
+    }
 }
 
 
